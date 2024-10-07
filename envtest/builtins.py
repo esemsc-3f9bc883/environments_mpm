eb0deb3bc55038963a2cf2285e6bceefbdeaabb0
acse-vf422 <vera.fan23@imperial.ac.uk>
2024-10-07 15:49:30 +0100

    add func

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
 
 __all__ = ['rand_array', 'smooth_image']
@@ -11,3 +12,9 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*
+
+def dummy_func(data):
+    df = pd.DataFrame(data, columns=['Values'])
+    mean = df['Values'].mean()
+    std_dev = df['Values'].std()
+    return mean, std_dev 
